9ac6509c33f1588cc1d2c2cb58f51ac7652b8850
Joseph Benoy <josephbenoy03@gmail.com>
2021-07-09 19:53:28 +0530

    changes to poll success ui

--- ui/src/components/pollsuccess/pollsuccess.js
+++ ui/src/components/pollsuccess/pollsuccess.js
@@ -4,8 +4,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import { Typography } from '@material-ui/core';
 import { useHistory } from 'react-router';
-
-
+import { Grid } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
    
    function getModalStyle() {
        return {
@@ -44,8 +45,16 @@ const PollSuccess = ({title,urlSlag,open,cb})=>{
                      <div style={modalStyle} className={classes.paper}>
                             <Typography variant="h5">{title}</Typography>
                             <Typography  variant="body1">{`Your poll is now online. Share the following link with your audience.`}</Typography>
-                            <Typography style={{backgroundColor:"rgba(0,0,0,0.1)",borderRadius:"3px",padding:"1% 2%",color:"rgba(0,0,0,0.8)",fontFamily:"monospace",margin:"2vh"}} variant="body2">{`easypolls.com/vote/${urlSlag}`}</Typography>
-                            <Button variant="contained" color="secondary" onClick={cb}>close</Button>
+                            <Typography style={{backgroundColor:"rgba(0,0,0,0.1)",borderRadius:"3px",padding:"0.5% 1%",color:"rgba(0,0,0,0.8)",fontFamily:"monospace",margin:"2vh",display:"table"}} variant="body2">{`easypolls.com/vote/${urlSlag}`}        
+                                   <IconButton aria-label="copy">
+                                          <FileCopyIcon fontSize="small" />
+                                   </IconButton>
+                            </Typography>
+                            <Grid container>
+                                   <Grid item xs={12} container justify="flex-end">
+                                          <Button variant="contained" color="primary" onClick={cb}>Close</Button>
+                                   </Grid>                            
+                            </Grid>
                      </div>
               </Modal>
        );
