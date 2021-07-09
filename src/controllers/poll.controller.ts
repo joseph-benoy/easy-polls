bf46717fa316c7966c433030aaa3a32b9c28fee0
Joseph Benoy <josephbenoy03@gmail.com>
2021-07-09 16:26:58 +0530

    updates to poll controller

--- src/controllers/poll.controller.ts
+++ src/controllers/poll.controller.ts
@@ -4,29 +4,33 @@ import Poll from '../models/poll.model';
 
 export default {
        createPoll:async(req:Request,res:Response,next:NextFunction)=>{
-              console.log("###");
-              var slag = await bcrypt.hash(req.body.title,1);
-              let poll = new Poll({
-                     title:req.body.title,
-                     description:req.body.description,
-                     expiry:req.body.expiry,
-                     options:JSON.stringify(req.body.options),
-                     // @ts-ignore
-                     createdBy:req.id,
-                     slag:slag
-              });
-              poll.validate()
-              .then(()=>{
-                     poll.save()
-                     .then((value:any)=>{
-                            res.json(value)
+              try{
+                     var slag = await bcrypt.hash(req.body.title,1);
+                     let poll = new Poll({
+                            title:req.body.title,
+                            description:req.body.description,
+                            expiry:req.body.expiry,
+                            options:JSON.parse(req.body.options),
+                            // @ts-ignore
+                            createdBy:req.id,
+                            slag:slag
+                     });
+                     poll.validate()
+                     .then(()=>{
+                            poll.save()
+                            .then((value:any)=>{
+                                   res.json(value)
+                            })
+                            .catch((err:any)=>{
+                                   next(err);
+                            })
                      })
                      .catch((err:any)=>{
                             next(err);
                      })
-              })
-              .catch((err:any)=>{
+              }
+              catch(err){
                      next(err);
-              })
+              }
        }
 }
